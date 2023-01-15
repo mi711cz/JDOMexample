9798915efbc98d5584792f627c2a7fe6257bf01e
mi711cz <cobranocka@gmail.com>
2023-01-15 12:33:53 +0100

    Read all elements and tags without getElementsByTagName

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -29,35 +29,14 @@ public class App
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    System.out.println("Student roll no : "
-                            + eElement.getAttribute("rollno"));
-                    System.out.println("Student entry : "
-                            + eElement.getAttribute("entry"));
-                    System.out.println("First Name : "
-                            + eElement
-                            .getElementsByTagName("firstname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Last Name : "
-                            + eElement
-                            .getElementsByTagName("lastname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Nick Name : "
-                            + eElement
-                            .getElementsByTagName("nickname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Marks : "
-                            + eElement
-                            .getElementsByTagName("marks")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Birthday : "
-                            + eElement
-                            .getElementsByTagName("birthday")
-                            .item(0)
-                            .getTextContent());
+                    NodeList nameList = eElement.getChildNodes();
+                    for (int j=0; j<nameList.getLength(); j++){
+                        Node n = nameList.item(j);
+                        if(n.getNodeType()==Node.ELEMENT_NODE){
+                            Element name = (Element) n;
+                            System.out.println("Tag " + name.getTagName() + ": " + name.getTextContent());
+                        }
+                    }
                 }
             }
         } catch (Exception e) {
